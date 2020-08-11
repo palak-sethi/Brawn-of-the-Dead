472cca13d46c30518ee814a5e04a640d991a10d0
Palak <palaksethi2001@gmail.com>
2020-08-11 14:24:24 +0530

    Initial Commit

---
+++ android/src/com/coinmaster/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.coinmaster.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.coinmaster.game.CoinMaster;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new CoinMaster(), config);
+	}
+}
